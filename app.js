472005bf698d9f4d323cbd7df8ad8277b2d0dd90
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-04-02 13:51:16 +0200

    Added a regular expression and function that turns bullet points and <br> into li's

--- app.js
+++ app.js
@@ -30,24 +30,62 @@ app.get("/samenwerken", (req, res) => {
 
       const minifiedHtml = normalHtml.replace(rx2, "")
 
-      let ts = removeEmpty(minifiedHtml);
+      let smallerHtml = removeEmpty(minifiedHtml);
 
+      let littleBetter = makeUlLi(smallerHtml)
 
+      res.send(littleBetter)
+      console.log(littleBetter)
+      // console.log(tst)
+      // console.log(smallerHtml)
     })
   })
 })
 
-function removeEmpty(txt) {
-  const rx = /\<(div|span|h[1-6])\s*(.[^\<])*\>[\s\t]*\<\/{1}\1\>/g;
+function removeEmpty(html) {
+  const rx = /\<(div|span|h[1-6])\s*(?:.[^\<])*\>[\s\t]*\<\/{1}\1\>/g;
 
-  if (rx.test(txt) === true) {
-    let newTxt = txt.replace(rx, "")
+  if (rx.test(html) === true) {
+    let newHtml = html.replace(rx, "")
     console.log("loop over again")
-    return removeEmpty(newTxt)
+    return removeEmpty(newHtml)
   } else {
     console.log("ending")
-    return txt;
+    return html;
   }
+}
+
+function makeUlLi(html) {
+  // Full regex
+  const rx = /(?:\<(div)\s*(?:.[^\<])*\>)?[\s\t\n]*(•).+?(?:\<\/(div)|(br\s*\/{0,1}))?\>/g;
+
+  html = html.replace(rx, (...args) => {
+    let match = args[0];
+    const firstDiv = args[1];
+    const bullet = args[2];
+    const lastDiv = args[3];
+    const br = args[4];
+
+    if (firstDiv !== undefined) {
+      match = match.replace(firstDiv, "ul")
+    }
+
+    if (bullet !== undefined) {
+      match = match.replace(bullet, "<li>")
+    }
+
+    if (lastDiv !== undefined) {
+      match = match.replace(lastDiv, "li></ul")
+    }
+
+    if (br !== undefined) {
+      match = match.replace(br, "/li")
+    }
+
+    return match
+  })
+
+  return html
 }
 
 app.listen(port, () => console.log(`Listening to port: ${port}`))
